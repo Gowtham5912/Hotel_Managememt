18018f0551bf6eef506724b4ce0e5581d742a5dd
Gowtham D V <sonugowda20040607@gmail.com>
2026-03-02 12:53:27 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../components/Hero' 
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiceOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <FeaturedDestination />
+        <ExclusiceOffers />
+        <Testimonial />
+        <NewsLetter />
+    </>
+  )
+}
+
+export default Home
